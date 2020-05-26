5ece64a6bcb6bf737a728ed5b81dd14b8c69eee8
geoallenrh <geoffallen@redhat.com>
2020-05-26 12:45:04 -0500

    super duper

--- src/main/java/org/acme/spring/web/GreetingBean.java
+++ src/main/java/org/acme/spring/web/GreetingBean.java
@@ -22,6 +22,6 @@ import org.springframework.stereotype.Service;
 public class GreetingBean {
 
     public String greet(String input) {
-        return "HELLO " + input.toUpperCase() + ", It's a terrific Tuesday!";
+        return "HELLO " + input.toUpperCase() + ", It's a super duper day!";
     }
 }
